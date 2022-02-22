64dc550db3aaf247d9bd4616b9b1de32d2c37463
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-02-22 15:39:23 -0800

    commit tree 98e9a2517895d65ee2c08dd36e3643cb4c14c327

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
-
+    def name=(input)
+        @name = input
+    end
+    def name
+        @name
+    end
+    def breed=(breed_input)
+        @breed = breed_input
+    end
+    def breed
+        @breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
 class Person
-
+    def name=(input)
+        @name = input
+    end
+    def name
+        @name
+    end
+    def job=(job_input)
+        @job = job_input
+    end
+    def job
+        @job
+    end
 end
